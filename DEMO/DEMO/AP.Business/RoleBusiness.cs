694e4c78c67796b28f14811dde7527a81d3c5a5c
JefersePasos <dpasos34819@ufide.ac.cr>
2026-06-15 02:04:32 -0600

    Implemantacion de roles

---
+++ DEMO/DEMO/AP.Business/RoleBusiness.cs
@@ -0,0 +1,39 @@
+﻿using AP.Data;
+using AP.Repositories;
+using System.Collections.Generic;
+
+namespace AP.Business
+{
+    public class RoleBusiness
+    {
+        private readonly RoleRepository _roleRepository;
+
+        public RoleBusiness()
+        {
+            _roleRepository = new RoleRepository();
+        }
+
+        public IEnumerable<Role> GetRoles(int id = 0)
+        {
+            if (id == 0)
+                return _roleRepository.GetAll();
+            else
+                return new List<Role> { _roleRepository.GetById(id) };
+        }
+
+        public bool SaveOrUpdate(Role role)
+        {
+            if (role.RoleId == 0)
+                _roleRepository.Add(role);
+            else
+                _roleRepository.Update(role);
+
+            return true;
+        }
+
+        public void Delete(int id)
+        {
+            _roleRepository.Delete(id);
+        }
+    }
+}
---
+++ DEMO/DEMO/AP.MVC/Controllers/RolesController.cs
@@ -0,0 +1,86 @@
+﻿using AP.Business;
+using AP.Data;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AP.MVC.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleBusiness _roleBusiness = new RoleBusiness();
+
+        public ActionResult Index()
+        {
+            return View(_roleBusiness.GetRoles());
+        }
+
+        public ActionResult Details(int id)
+        {
+            var role = _roleBusiness.GetRoles(id).FirstOrDefault();
+
+            if (role == null)
+                return HttpNotFound();
+
+            return View(role);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                _roleBusiness.SaveOrUpdate(role);
+                return RedirectToAction("Index");
+            }
+
+            return View(role);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var role = _roleBusiness.GetRoles(id).FirstOrDefault();
+
+            if (role == null)
+                return HttpNotFound();
+
+            return View(role);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                _roleBusiness.SaveOrUpdate(role);
+                return RedirectToAction("Index");
+            }
+
+            return View(role);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var role = _roleBusiness.GetRoles(id).FirstOrDefault();
+
+            if (role == null)
+                return HttpNotFound();
+
+            return View(role);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _roleBusiness.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
